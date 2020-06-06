5f422cf81b0f619cbf028411fa0142602765b390
mattjmcnaughton <mattjmcnaughton@gmail.com>
2020-06-05 21:53:28 -0400

    Add more fundamentals of zfs-nas role
    Add more components of the `zfs-nas` role. Specifically, add the
    components that actually create the `zpool` and `zfs datasets`.
    
    The `zfs` support is ok... we can create datasets via Ansible, but not
    zpool. W.r.t. inspec, we need to use command, as we can't use the
    `inspec` ZFS resources. See https://github.com/inspec/inspec/issues/5075
    for more details.

--- provisioning/roles/mattjmcnaughton.zfs-nas/test/integration/default/spec.rb
+++ provisioning/roles/mattjmcnaughton.zfs-nas/test/integration/default/spec.rb
@@ -12,3 +12,19 @@ control 'zfs-nas-01' do
     end
   end
 end
+
+control 'zfs-nas-02' do
+  title 'ZFS'
+  impact 1.0
+
+  # We can't use the `zfs_pool` and `zfs_dataset` resources w/ inspec bc they
+  # only work on FreeBSD.
+
+  describe command("zpool get -Hp all tank") do
+    its('exit_status') { should eq 0 }
+  end
+
+  describe command("zfs get -Hp all tank/test") do
+    its('exit_status') { should eq 0 }
+  end
+end
