e2b2e4213cea8e73f237bf1a3fd34669036ab293
mattjmcnaughton <mattjmcnaughton@gmail.com>
2021-09-29 10:03:23 -0400

    Initial home assistant container config
    Base role for deploying the initial home assistant container.
    
    Simple tests via inspec.
    
    Once I have home-assistant launched, I can think about a back
    up/automation strategy. In good news, it looks like pretty much all components of it
    are stored in the `/config` dir, which is good.

--- provisioning/roles/mattjmcnaughton.home-assistant/README.md
+++ provisioning/roles/mattjmcnaughton.home-assistant/README.md
@@ -1,3 +1,10 @@
 # mattjmcnaughton.home-assistant
 
 Role for deploying home-assistant on the Raspberry Pi.
+
+## TODO:
+
+- Place `nginx` in front of nginx (w/ mattjmcnaughton.io cert).
+  - For now, can access via port-forwarding over ssh?
+- Determine backup plan for data
+- Configure more of `home-assistant` via ansible (i.e. user provisioning, etc...)
--- provisioning/roles/mattjmcnaughton.home-assistant/test/integration/default/spec.rb
+++ provisioning/roles/mattjmcnaughton.home-assistant/test/integration/default/spec.rb
@@ -3,12 +3,5 @@ describe docker_container('homeassistant') do
   it { should be_running }
   its('id') { should_not eq '' }
   its('image') { should include 'homeassistant' }
-
-  # Need more tests
-end
-
-describe service('nginx') do
-  it { should be_installed }
-  it { should be_enabled }
-  it { should be_running }
+  its('ports') { should eq '' }  # No ports because set `host=net`.
 end
